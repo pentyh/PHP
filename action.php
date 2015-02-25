41763405c4bedc49e1b81736a6265d9302211691
김일룡 <jirilong@thatsgood.com>
2015-02-25 18:06:51 +0900

    - rl

--- action.php
+++ action.php
@@ -1,5 +1,17 @@
 <?php
-    
-    echo "do action!";
-    
+
+$fdset = array('1', '2', '3', '4', '5');
+
+$gfile = 'config.ini';
+$fp = fopen($gfile,'w');
+
+foreach ($fdset as $val)
+{
+	fwrite($fp, $val."=".trim(${$val})."\n");
+}
+
+fclose($fp);
+@chmod($gfile,0707);
+
+
 ?>
--- index.php
+++ index.php
@@ -1,44 +1,38 @@
 <?php
-    
-    $myfile = fopen("config.ini", "r") or die("Unable to open file!");
-    
-    //$data = fread($myfile,filesize("config.ini"));
-    //fclose($myfile);
-    
-?>
 
+$gfile = 'config.ini';
+$myfile = fopen($gfile, "r") or die("Unable to open file!");
+
+//$data = fread($myfile,filesize("config.ini"));
+//fclose($myfile);
+?>
 
 <!DOCTYPE HTML>
 <html>
-<body>
+	<body>
 
-<form action="/action.php" method="get">
+		<form action="action.php" method="get">
+			<table style="width:100%">
+				<?php while(!feof($myfile)):
+				?>
+				<?php
+				$row = fgets($myfile);
+				$n = strpos($row, '=');
+				$key = substr($row, 0, $n);
+				$value = substr($row, $n + 1);
+				?>
+					<tr>
+						<td><?php echo $key ?></td>
+						<td><input type="text" name="<?php echo $key ?>" value="<?php echo $value ?>"></td>
+					</tr>
+				<?php endwhile ?>
+			</table>
+			<br />
+			<input type="submit" value="SAVE"/>
+			<input type="reset" value="RESET"/>
+		</form>
 
-<table style="width:100%">
-
-<?php while(!feof($myfile)):?>
-<?php
-    $row = fgets($myfile);
-    $n=strpos($row,'=');
-    $key=substr($row,0,$n);
-    $value=substr($row,$n+1);
-?>
-
-<tr>
-<td><?php echo $key ?></td>
-<td>
-<input type="text" value="<?php echo $value ?>">
-</td>
-</tr>
-<?php endwhile ?>
-
-</table><br />
-
-<input type="submit" value="SAVE"/>
-<input type="reset" value="RESET"/>
-</form>
-
-</body>
+	</body>
 </html>
 
 <?php fclose($myfile); ?>
